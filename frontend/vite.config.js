f085dd9cfb5f6d6f857f38d45d681b1570f6c838
M Adithya Rao <166639532+Aditya-rao-1@users.noreply.github.com>
2025-08-31 19:54:38 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
     }
   }
-=======
->>>>>>> 924bd47bb92d50694c2e5d84c1eb9b279d77295f
+
 })
